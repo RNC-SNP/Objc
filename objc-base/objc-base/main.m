302b68f6948690943fcd2d338312e664f1f065c2
Rinc <i@rincliu.com>
2014-04-08 01:32:44 +0800

    Added a demo of 'Associated'.

--- objc-base/objc-base/main.m
+++ objc-base/objc-base/main.m
@@ -11,6 +11,7 @@
 
 int main(int argc, const char* argv[]){
     @autoreleasepool {
+        
         // Use Block:
         int (^SquareBlock)(int) = ^(int x){
             int result = x * x;
@@ -18,7 +19,19 @@ int main(int argc, const char* argv[]){
         };
         NSLog(@"Result of SquareBlock(9): %d", SquareBlock(9));
         
+        
+        // Use Associate:
+        static char key;
+        NSArray *sourceObj= [[NSArray alloc]initWithObjects:@"Android", @"iOS", @"WindowsPhone", nil];
+        NSString *associatedObj = [[NSString alloc]initWithFormat:@"%@", @"Three mobile platforms"];
+        objc_setAssociatedObject(sourceObj, key, associatedObj, OBJC_ASSOCIATION_RETAIN);
+        // [associatedObj release];
+        NSLog(@"associatedObj: %@", (NSString *)objc_getAssociatedObject(sourceObj, key));
+        objc_removeAssociatedObjects(sourceObj);
+        NSLog(@"associatedObj: %@", (NSString *)objc_getAssociatedObject(sourceObj, key));
+        // [sourceObj release];
     }
+    
     return 0;
 }
 
