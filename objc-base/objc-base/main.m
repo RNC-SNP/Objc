69552310a408e6b9269d6688f42a339a876a6bb8
Rinc <i@rincliu.com>
2014-04-19 18:01:17 +0800

    'for...in' expression.

--- objc-base/objc-base/main.m
+++ objc-base/objc-base/main.m
@@ -12,6 +12,7 @@
 int main(int argc, const char* argv[]){
     @autoreleasepool {
         
+        
         // Use Block:
         int (^SquareBlock)(int) = ^(int x){
             int result = x * x;
@@ -30,6 +31,28 @@ int main(int argc, const char* argv[]){
         objc_removeAssociatedObjects(sourceObj);
         NSLog(@"associatedObj: %@", (NSString *)objc_getAssociatedObject(sourceObj, key));
         // [sourceObj release];
+        
+        
+        // Use 'for...in' expression with Array:
+        NSArray *array = [NSArray arrayWithObjects: @"Android", @"iOS", @"WinPhone", nil];
+        for (NSString *ele in array) {
+            NSLog(@"element in array: %@", ele);
+        }
+        // Use 'for...in' expression with Dictionary:
+        NSDictionary *dic = [NSDictionary dictionaryWithObjectsAndKeys: @"Java", @"Android", @"Objective-C", @"iOS", @"C#", @"WinPhone", nil];
+        NSString *_key;
+        for (_key in dic) {
+            NSLog(@"OS: %@, lang: %@", _key, [dic objectForKey:_key]);
+        }
+        // Use 'for...in' expression with NSEnumerator:
+        NSEnumerator *enumerator = [array objectEnumerator];
+        for(NSString *str in enumerator) {
+            NSLog(@"Current: %@", str);
+            NSString *next = [enumerator nextObject];
+            if(next != nil){
+                NSLog(@"Next: %@", next);
+            }
+        }
     }
     
     return 0;
