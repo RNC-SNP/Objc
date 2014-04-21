9a0fb7fc329474a30974e55f81f05c2d9c19a573
Rinc <i@rincliu.com>
2014-04-22 00:59:00 +0800

    Use basic Data Types defined in Foundation.

--- objc-base/objc-base/main.m
+++ objc-base/objc-base/main.m
@@ -11,6 +11,101 @@
 
 int main(int argc, const char* argv[]){
     @autoreleasepool {
+        
+        
+        // Use NSString:
+        NSString *nameStr = @"Rinc";
+        NSString *GreetingStr = [[NSString alloc] initWithFormat: @"Hello, %@", nameStr];
+        NSString *utf8 = [NSString stringWithUTF8String:"iOS 7应用开发技术详解"];
+        // Travel NSString:
+        for(int i=0; i < [utf8 length]; i++) {
+            NSLog(@"%c", [utf8 characterAtIndex:i]);
+        }
+        // Use NSMutableString:
+        NSMutableString *mStr = [NSMutableString stringWithString:nameStr];
+        [mStr appendString:@" Liu"];
+        NSRange range = [mStr rangeOfString:@" Liu"];
+        // Use NSMutableString:
+        NSMutableString *subStr1 = [mStr substringWithRange:range];
+        NSMutableString *subStr2 = [mStr substringToIndex:3];
+        [mStr replaceCharactersInRange:range withString:@" Lew"];
+        
+        
+        // Use NSNumber:
+        NSNumber *num1 = @0.1234F;
+        float f = 5.6789;
+        NSNumber *num2 = [[NSNumber alloc] initWithFloat: f];
+        NSNumber *num3 = [NSNumber numberWithBool:YES];
+        bool b = [num3 boolValue];
+        
+        
+        // Use Array:
+        NSArray *array1 = @[@"Android", @"iOS", @"WinPhone"];
+        NSArray *array2 = [[NSArray alloc] initWithObjects:@"Android", @"iOS", @"WinPhone", nil];
+        NSString *objs[3] = {@"Android", @"iOS", @"WinPhone"};
+        NSArray *array3 = [NSArray arrayWithObjects:&(*objs) count:3];
+        NSString *firstEle = [array1 firstObject];
+        // Copy array:
+        NSArray *array4 = [NSArray arrayWithArray:array3];
+        // Sort array:
+        [array4 sortedArrayUsingSelector:@selector(compare:)];
+        // Travel array:
+        for (NSString *ele in array3) {
+            NSLog(@"element in array: %@", ele);
+        }
+        // Array in Array:
+        NSArray *xArray = [NSArray arrayWithObjects:
+                           [NSArray arrayWithObjects:@"A0", @"B0", @"C0", nil],
+                           [NSArray arrayWithObjects:@"A1", @"B1", @"C1", nil],
+                           [NSArray arrayWithObjects:@"A2", @"B2", @"C2", nil],
+                           nil];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                NSLog(@"%@", [[xArray objectAtIndex:i] objectAtIndex:j]);
+            }
+        }
+        // Use NSMutableArray:
+        NSMutableArray *mArray = [NSMutableArray arrayWithCapacity:10];
+        [mArray addObject:@"BlackBerry"];
+        [mArray insertObject:@"Symbian" atIndex:1];
+        [mArray replaceObjectAtIndex:1 withObject:@"WindowsMobile"];
+        
+        
+        // Use Dictionary:
+        NSDictionary *dic1 = @{@"key1":@"value1", @"key2":@"value2"};
+        NSDictionary *dic2 = [NSDictionary dictionaryWithObjectsAndKeys: @"Java", @"Android", @"Objective-C", @"iOS", @"C#", @"WinPhone", nil];
+        NSArray *valueArray = [NSArray arrayWithObjects:@"Java", @"Objective-C", @"C#", nil];
+        NSArray *keyArray = [NSArray arrayWithObjects:@"Android", @"iOS", @"WinPhone", nil];
+        NSDictionary *dic3 = [NSDictionary dictionaryWithObjects:valueArray forKeys:keyArray];
+        // Travel Dictionary:
+        NSString *_key;
+        for (_key in dic3) {
+            NSLog(@"OS: %@, lang: %@", _key, [dic3 objectForKey:_key]);
+        }
+        // Travel with NSEnumerator(like Iterator in Java):
+        NSEnumerator *keyEnum = dic3.keyEnumerator;
+        for(NSString *_k in keyEnum){
+            NSLog(@"OS: %@, lang: %@", _k, [dic3 objectForKey:_k]);
+        }
+        // Use NSMutableDictionary:
+        NSMutableDictionary *mDic = [[NSMutableDictionary alloc] init];
+        mDic[@"WindowsMobile"] = @"C#";
+        [mDic setObject:@"C#" forKey:@"WindowsMobile"];
+        
+        
+        // Use NSDate/NSDateFormatter:
+        NSDate *currentDate = [NSDate date];
+        NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+        [formatter setDateFormat: @"yyyy-MM-dd HH:mm:ss"];
+        NSLog(@"Current time: %@", [formatter stringFromDate: currentDate]);
+        // Use NSCalendar/NSTimeZone/NSDateComponents:
+        NSCalendar *calendar = [[NSCalendar alloc] initWithCalendarIdentifier: NSGregorianCalendar];
+        [calendar setTimeZone:[NSTimeZone systemTimeZone]];
+        NSCalendarUnit unit = NSYearCalendarUnit | NSMonthCalendarUnit | NSDayCalendarUnit | NSHourCalendarUnit | NSMinuteCalendarUnit | NSSecondCalendarUnit;
+        NSDateComponents *dc = [calendar components: unit fromDate: currentDate];
+        NSLog(@"Current time: %d-%d-%d %d:%d:%d", [dc year], [dc month], [dc day], [dc hour], [dc minute], [dc second]);
+        
+        
         // Use Block as function:
         double one = 1;
         double (^MultiplyBlock)(double, double) = ^(double x, double y){
@@ -33,23 +128,11 @@ int main(int argc, const char* argv[]){
         // [sourceObj release];
         
         
-        // Use 'for...in' expression with Array:
-        NSArray *array = [NSArray arrayWithObjects: @"Android", @"iOS", @"WinPhone", nil];
-        for (NSString *ele in array) {
-            NSLog(@"element in array: %@", ele);
-        }
-        // Use 'for...in' expression with Dictionary:
-        NSDictionary *dic = [NSDictionary dictionaryWithObjectsAndKeys: @"Java", @"Android", @"Objective-C", @"iOS", @"C#", @"WinPhone", nil];
-        NSString *_key;
-        for (_key in dic) {
-            NSLog(@"OS: %@, lang: %@", _key, [dic objectForKey:_key]);
-        }
-        // Use 'for...in' expression with NSEnumerator(Just like Iterator in Java):
-        NSEnumerator *_enum= [array objectEnumerator];
+        // Use try/catch/finally/throw:
+        NSEnumerator *_enum= [array2 objectEnumerator];
         for(NSString *str in _enum) {
             NSLog(@"Current: %@", str);
             NSString *next = [_enum nextObject];
-            // Use try/catch/finally/throw:
             @try {
                 if(next) {
                     NSLog(@"Next: %@", next);
@@ -62,7 +145,7 @@ int main(int argc, const char* argv[]){
                 NSLog(@"%@ occured: %@", [exception name], [exception reason]);
             }
             @finally {
-                //TODO
+                NSLog(@"Final operation...");
             }
         }
     }
---
+++ objc-foundation/objc-foundation/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  objc-foundation
+//
+//  Created by RincLiu on 4/21/14.
+//  Copyright (c) 2014 Rinc Liu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
