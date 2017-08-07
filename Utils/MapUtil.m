e8d441d97ba02d64ce7be815620b8dbcf8a7885c
Rinc Liu <i@rinc.xyz>
2017-08-07 07:03:19 -0500

    Update MapUtil.m

--- Utils/MapUtil.m
+++ Utils/MapUtil.m
@@ -6,26 +6,31 @@ static NSMutableArray<NSString*>* supportedMaps;
 @implementation MapUtil
 
 NSString* const MAP_DEFAULT = @"MAP_DEFAULT";
+NSString* const MAP_TENCENT = @"MAP_TENCENT";
 NSString* const MAP_GOOGLE = @"MAP_GOOGLE";
-NSString* const MAP_BAIDU = @"MAP_BAIDU";
 NSString* const MAP_GAODE = @"MAP_GAODE";
+NSString* const MAP_BAIDU = @"MAP_BAIDU";
 
+NSString* const SCHEME_MAP_TENCENT = @"qqmap://";
 NSString* const SCHEME_MAP_GOOGLE = @"comgooglemaps://";
-NSString* const SCHEME_MAP_BAIDU = @"baidumap://";
 NSString* const SCHEME_MAP_GAODE = @"iosamap://";
+NSString* const SCHEME_MAP_BAIDU = @"baidumap://";
 
 +(NSArray<NSString*>*)supportedMaps {
     if (supportedMaps == nil) {
         supportedMaps = [NSMutableArray new];
         [supportedMaps addObject:MAP_DEFAULT];
+        if ([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:SCHEME_MAP_TENCENT]]) {
+            [supportedMaps addObject:MAP_TENCENT];
+        }
         if ([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:SCHEME_MAP_GOOGLE]]) {
             [supportedMaps addObject:MAP_GOOGLE];
         }
-        if ([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:SCHEME_MAP_BAIDU]]) {
-            [supportedMaps addObject:MAP_BAIDU];
-        }
         if ([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:SCHEME_MAP_GAODE]]) {
             [supportedMaps addObject:MAP_GAODE];
+        }
+        if ([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:SCHEME_MAP_BAIDU]]) {
+            [supportedMaps addObject:MAP_BAIDU];
         }
     }
     return supportedMaps;
@@ -37,20 +42,23 @@ NSString* const SCHEME_MAP_GAODE = @"iosamap://";
     [mapItem openInMapsWithLaunchOptions:[self defaultLaunchOptions]];
 }
 
-+(void)navigateToLocation:(CLLocationCoordinate2D)location Map:(NSString*)map {
++(void)navigateToLocation:(CLLocationCoordinate2D)location Title:(NSString*)title Map:(NSString*)map {
     if ([[self supportedMaps]containsObject:map]) {
         if ([MAP_DEFAULT isEqualToString:map]) {
             [MKMapItem openMapsWithItems:@[[MKMapItem mapItemForCurrentLocation], [self MapItemFromLocation:location]]
                            launchOptions:[self defaultLaunchOptions]];
+        } else if ([MAP_TENCENT isEqualToString:map]) {
+            NSString *url = @"%@map/routeplan?from=我的位置&type=walk&tocoord=%f,%f&to=%@&coord_type=1&policy=0";
+            [self openMapUrl:[NSString stringWithFormat:url, SCHEME_MAP_TENCENT, location.latitude, location.longitude, title]];
         } else if ([MAP_GOOGLE isEqualToString:map]){
             NSString *url = @"%@?x-source=%@&x-success=%@&saddr=&daddr=%f,%f&directionsmode=driving";
             [self openMapUrl:[NSString stringWithFormat:url, SCHEME_MAP_GOOGLE, @"叮当", @"dingdang://", location.latitude, location.longitude]];
-        } else if ([MAP_BAIDU isEqualToString:map]){
-            NSString *url = @"%@map/direction?origin={{我的位置}&destination=latlng:%f,%f|name=目的地&mode=driving&coord_type=gcj02";
-            [self openMapUrl:[NSString stringWithFormat:url, SCHEME_MAP_BAIDU, location.latitude, location.longitude]];
         } else if ([MAP_GAODE isEqualToString:map]){
             NSString *url = @"%@navi?sourceApplication=%@&backScheme=%@&lat=%f&lon=%f&dev=0&style=2";
             [self openMapUrl:[NSString stringWithFormat:url, SCHEME_MAP_GAODE, @"叮当", @"dingdang://", location.latitude, location.longitude]];
+        } else if ([MAP_BAIDU isEqualToString:map]){
+            NSString *url = @"%@map/direction?origin={{我的位置}&destination=latlng:%f,%f|name=目的地&mode=driving&coord_type=gcj02";
+            [self openMapUrl:[NSString stringWithFormat:url, SCHEME_MAP_BAIDU, location.latitude, location.longitude]];
         }
     }
 }
