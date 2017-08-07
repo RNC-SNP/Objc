7920d1d1b2aea0b86747c979f2818ae3b65f04fc
Rinc Liu <i@rinc.xyz>
2017-08-06 21:15:18 -0500

    Update UIAlertController+Window.m

--- Utils/UIAlertController+Window.m
+++ Utils/UIAlertController+Window.m
@@ -30,12 +30,12 @@
 - (void)show:(BOOL)animated {
     self.alertWindow = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
     self.alertWindow.rootViewController = [[UIViewController alloc] init];
-    id<UIApplicationDelegate> delegate = [UIApplication sharedApplication].delegate;
+    /*id<UIApplicationDelegate> delegate = [UIApplication sharedApplication].delegate;
     // Applications that does not load with UIMainStoryboardFile might not have a window property:
     if ([delegate respondsToSelector:@selector(window)]) {
         // we inherit the main window's tintColor
         self.alertWindow.tintColor = delegate.window.tintColor;
-    }
+    }*/
     // window level is above the top window (this makes the alert, if it's a sheet, show over the keyboard)
     UIWindow *topWindow = [UIApplication sharedApplication].windows.lastObject;
     self.alertWindow.windowLevel = topWindow.windowLevel + 1;
